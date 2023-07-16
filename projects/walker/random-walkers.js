4bcde5b9709ecd692d8a8bb60b294e646e7ca2f6
loganaros <logabeast11@gmail.com>
2023-07-15 19:31:34 -0700

    responsive canvas

--- projects/walker/random-walkers.js
+++ projects/walker/random-walkers.js
@@ -73,7 +73,7 @@ let flowField;
 let fr;
 
 function setup() {
-  var canvas = createCanvas(750, 250);
+  var canvas = createCanvas(500, 500);
   canvas.parent('card--spotify--all');
   inc = 0.1;
   scl = 10;
